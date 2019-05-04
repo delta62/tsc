aec0dce2dde0916e08e8bc78f42a2e9d2019a9ed
Sam Noedel <sam.noedel@gmail.com>
2019-05-03 20:06:26 -0700

    Build out lexer results

--- src/lexer.rs
+++ src/lexer.rs
@@ -3,6 +3,7 @@ use std::str::Chars;
 
 enum TokenType {
     Null,
+    String(String),
 }
 
 struct Token {
@@ -23,9 +24,20 @@ struct Lexer<'input> {
     stream: Peekable<Chars<'input>>,
 }
 
-struct ParseError {
+enum LexError {
+    UnexpectedEndOfInput(Location),
+    UnexpectedCharacter(Location),
+}
+
+struct Location {
     column: u32,
     line: u32,
+}
+
+impl Location {
+    fn new(line: u32, column: u32) -> Location {
+        Location { line, column }
+    }
 }
 
 impl<'input> Lexer<'input> {
@@ -37,25 +49,60 @@ impl<'input> Lexer<'input> {
         }
     }
 
-    fn lex_string(&mut self, delimeter: char) -> Result<String, ParseError> {
+    fn get_location(self) -> Location {
+        Location::new(self.line, self.column)
+    }
+
+    fn peek(self) -> Option<&'input char> {
+        self.stream.peek()
+    }
+
+    fn escape_sequence(self) -> Result<String, LexError> {
+        Err(LexError::UnexpectedEndOfInput(self.get_location()))
+    }
+
+    fn lex_string(&mut self, delimeter: char) -> Result<Token, LexError> {
         let mut s = String::new();
         s.push(delimeter);
+
         loop {
-            match self.next_char() {
+            let next = self.next_char();
+            match next {
                 Some(c) => {
                     s.push(c);
-                    if c == delimeter
+                    if c == delimeter {
                         break;
-                    if c == '\\':
-                        // Escape sequence
-                        // Line continuation
+                    }
+                    if c == '\\' {
+                        match self.peek() {
+                            // Escape sequence
+                            Some(_n) => {
+                                let mut seq = self.escape_sequence();
+                                match seq {
+                                    Ok(x) => s.push_str(&x),
+                                    Err(x) => return Err(x)
+                                }
+                            }
+                            // Line continuation
+                            // EOF
+                            None => {
+                                return Err(LexError::UnexpectedEndOfInput(self.get_location()))
+                            }
+                        }
+                    }
                 },
-                None => Error(ParseError { line: self.line, column: self.column })
+                None => {
+                    return Err(LexError::UnexpectedEndOfInput(self.get_location()))
+                }
             }
 
         }
         s.shrink_to_fit();
-        s
+        Ok(Token {
+            line: self.line,
+            column: self.column,
+            typ: TokenType::String(s)
+        })
     }
 
     fn next_char(&mut self) -> Option<char> {
@@ -75,15 +122,15 @@ impl<'input> Lexer<'input> {
 }
 
 impl<'input> Iterator for Lexer<'input> {
-    type Item = Token;
+    type Item = Result<Token, LexError>;
 
-    fn next(&mut self) -> Option<Token> {
+    fn next(&mut self) -> Option<Result<Token, LexError>> {
         match self.next_char() {
             Some('n') => {
-                Some(Token::new(self.line, self.column, TokenType::Null))
+                Some(Ok(Token::new(self.line, self.column, TokenType::Null)))
             },
-            Some('\'') => self.lex_string('\''),
-            Some('"') => self.lex_string('"'),
+            Some('\'') => Some(self.lex_string('\'')),
+            Some('"') => Some(self.lex_string('"')),
             _ => None
         }
     }
