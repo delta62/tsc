2900b2744a357fdeaf9eb1bf536b574ecf2d3a1e
Sam Noedel <sam.noedel@gmail.com>
2019-06-18 22:30:51 -0700

    Move error wrapper code around

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -29,17 +29,23 @@ where I: Iterator<Item = char> {
     }
 
     fn statement_list(&mut self) -> Result<Vec<Node>, ParseError> {
-        let stmts = Vec::new();
+        let mut stmts: Vec<Node> = Vec::new();
         loop {
             let next = self.lexer.next();
             match &next {
                 Some(Ok(token)) => {
-                    if is_statement_start(&token) {
-
-                    } else if is_declaration_start(&token) {
-
-                    } else {
-                        break
+                    let s = self.match_statement();
+                    match s {
+                        Some(Ok(s)) => stmts.push(s),
+                        Some(Err(_)) => return Err(ParseError::UnexpectedToken),
+                        None => {
+                            let d = self.match_declaration();
+                            match d {
+                                Some(Ok(d)) => stmts.push(d),
+                                Some(Err(_)) => return Err(ParseError::UnexpectedToken),
+                                None => break
+                            }
+                        }
                     }
                 },
                 Some(Err(_)) => return Err(ParseError::UnexpectedToken),
@@ -52,17 +58,30 @@ where I: Iterator<Item = char> {
         }
         Ok(stmts)
     }
-}
 
-fn is_statement_start(token: &Token) -> bool {
-    match token.typ {
-        TokenType::LeftBrace
-        | TokenType::Identifier(_, Some(ReservedWord::Var))
-        | TokenType::Semicolon => true,
-        _ => false,
+    fn match_statement(&mut self) -> Option<Result<Node, ParseError>> {
+        match self.lexer.next() {
+            Some(Ok(Token { typ: TokenType::LeftBrace, .. })) => {
+                Some(self.block())
+            },
+            Some(Err(_)) => return Some(Err(ParseError::UnexpectedToken)),
+            Some(_) => return Some(Err(ParseError::UnexpectedToken)),
+            None => None,
+        }
     }
-}
 
-fn is_declaration_start(_token: &Token) -> bool {
-    false
+    fn match_declaration(&mut self) -> Option<Result<Node, ParseError>> {
+        self.lexer.next().map(|token| {
+            token.map_err(|_| ParseError::UnexpectedToken)
+                .and_then(|token| self.let_declaration())
+        })
+    }
+
+    fn let_declaration(&mut self) -> Result<Node, ParseError> {
+        Err(ParseError::UnexpectedToken)
+    }
+
+    fn block(&mut self) -> Result<Node, ParseError> {
+        Err(ParseError::UnexpectedToken)
+    }
 }
