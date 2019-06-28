acbd83a76ad78b328fdf81a2c5effaa4da482fd4
Sam Noedel <sam.noedel@gmail.com>
2019-06-27 17:57:16 -0700

    Make extremely stupid SAP parser

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -5,16 +5,7 @@ extern crate lexer;
 mod errors;
 
 use errors::*;
-use lexer::{Lexer,ReservedWord,Token,TokenType};
-
-pub enum LetOrConst {
-    Let,
-    Const,
-}
-
-pub enum Declaration {
-    LexicalDeclaration(LetOrConst, Vec<Node>),
-}
+use lexer::{Lexer,TokenType};
 
 pub enum Assignment {
     ConditionalExpression,
@@ -24,242 +15,102 @@ pub enum Assignment {
     LeftHandSideExpression,
 }
 
-pub struct Binding {
-    identifier: String,
-    assignment: Option<Assignment>,
+pub struct Script {
+    pub body: Vec<StatementListItem>,
 }
 
-pub enum Node {
-    Binding(Binding),
-    BindingList(Vec<Node>),
+pub enum StatementListItem {
+    Statement(Statement),
     Declaration(Declaration),
-    Script(Vec<Node>),
-    Statement,
+}
+
+pub enum Statement {
+    BlockStatement,
+    VariableStatement,
+    EmptyStatement,
+    ExpressionStatement,
+    IfStatement,
+    BreakableStatement,
+    ContinueStatement,
+    BreakStatement,
+    ReturnStatement,
+    WithStatement,
+    LabelledStatement,
+    ThrowStatement,
+    TryStatement,
+    DebuggerStatement,
+}
+
+pub enum Declaration {
+    HoistableDeclaration(HoistableDeclaration),
+    ClassDeclaration,
+    LexicalDeclaration,
+}
+
+pub enum HoistableDeclaration {
+    FunctionDeclaration,
+    GeneratorDefinition,
+    AsyncFunctionDefinition,
+    AsyncGeneratorDefinition,
 }
 
 pub struct Parser<I>
 where I: Iterator<Item = char> {
-    lexer: Lexer<I>,
+    lexer:       Lexer<I>,
+    diagnostics: Vec<Error>,
 }
 
 impl<I> Parser<I>
 where I: Iterator<Item = char> {
     pub fn new(lexer: Lexer<I>) -> Parser<I> {
-        Parser { lexer: lexer }
+        Parser {
+            lexer:       lexer,
+            diagnostics: Vec::new(),
+        }
     }
 
-    pub fn script(&mut self) -> Result<Node> {
-        self.statement_list().map(|list| Node::Script(list))
+    pub fn diagnostics(&self) -> &Vec<Error> {
+        &self.diagnostics
     }
 
-    fn statement_list(&mut self) -> Result<Vec<Node>> {
-        let mut stmts: Vec<Node> = Vec::new();
+    pub fn script(&mut self) -> Script {
+        let mut statements = Vec::new();
+
         loop {
-            let next = self.lexer.next();
-            match &next {
-                Some(_) => {
-                    let result = self.declaration_or_statement();
-                    match result {
-                        Ok(x) => stmts.push(x),
-                        Err(e) => return Err(e),
+            match self.lexer.next() {
+                Some(t) => {
+                    match t.typ {
+                        TokenType::Semicolon => statements.push(StatementListItem::Statement(Statement::EmptyStatement)),
+                        _                    => self.diagnostics.push(ErrorKind::UnexpectedToken(t).into()),
                     }
                 },
-                _ => break
-            }
-
-            if next.is_none() {
-                break
+                None => break,
             }
         }
-        Ok(stmts)
-    }
 
-    fn declaration_or_statement(&mut self) -> Result<Node> {
-        let token = self.lexer.next().unwrap();
-        match &token {
-            // Declarations
-            x if is_variable_declaration(x)                   => self.variable_declaration(),
-            x if is_function_declaration(x)                   => self.function_declaration(),
-            x if is_class_declaration(x)                      => self.class_declaration(),
-            // Statements
-            x if is_variable_statement(x)                     => self.variable_declaration(),
-            x if x.typ == TokenType::LeftBrace                => self.block(),
-            x if x.typ == TokenType::Semicolon                => self.empty_statement(),
-            // ExpressionStatement
-            x if is_reserved_word(x, &ReservedWord::If)       => self.if_statement(),
-            x if is_reserved_word(x, &ReservedWord::Switch)   => self.switch_statement(),
-            x if is_reserved_word(x, &ReservedWord::Do)       => self.do_statement(),
-            x if is_reserved_word(x, &ReservedWord::While)    => self.while_statement(),
-            x if is_reserved_word(x, &ReservedWord::For)      => self.for_statement(),
-            x if is_reserved_word(x, &ReservedWord::Continue) => self.continue_statement(),
-            x if is_reserved_word(x, &ReservedWord::Break)    => self.break_statement(),
-            x if is_reserved_word(x, &ReservedWord::Return)   => self.return_statement(),
-            x if is_reserved_word(x, &ReservedWord::With)     => self.with_statement(),
-            // LabelledStatement
-            x if is_reserved_word(x, &ReservedWord::Throw)    => self.throw_statement(),
-            x if is_reserved_word(x, &ReservedWord::Try)      => self.try_statement(),
-            x if is_reserved_word(x, &ReservedWord::Debugger) => self.debugger_statement(),
-            // Other
-            _                                                 => Err(ErrorKind::NotImplemented.into()),
-        }
-    }
-
-    fn variable_declaration(&mut self) -> Result<Node> {
-        self.let_or_const()
-            .and_then(|kw| self.binding_list().map(|list| Declaration::LexicalDeclaration(kw, vec! [ list ])))
-            .map(|decl| Node::Declaration(decl))
-    }
-
-    fn let_or_const(&mut self) -> Result<LetOrConst> {
-        match self.lexer.next() {
-            Some(Token { column: _, line: _, typ: TokenType::Identifier(_, Some(ReservedWord::Let)) }) => {
-                Ok(LetOrConst::Let)
-            },
-            Some(Token { column: _, line: _, typ: TokenType::Identifier(_, Some(ReservedWord::Const)) }) => {
-                Ok(LetOrConst::Const)
-            },
-            Some(x) => Err(ErrorKind::UnexpectedToken(x).into()),
-            None => Err(ErrorKind::UnexpectedEof.into()),
-        }
-    }
-
-    fn binding_list(&mut self) -> Result<Node> {
-        let list = Vec::new();
-        let fst = self.lexical_binding();
-        Ok(Node::BindingList(list))
-    }
-
-    fn lexical_binding(&mut self) -> Result<Node> {
-        let identifier = match self.lexer.next() {
-            // Some(Ok(Token { column: _, line: _, typ: TokenType::Identifier(_, Some(ReservedWord::Yield)) })) => None,
-            // Some(Ok(Token { column: _, line: _, typ: TokenType::Identifier(_, Some(ReservedWord::Await)) })) => None,
-            // Some(Ok(t)) if is_reserved_word(t) => Err(ParseError::UnexpectedToken),
-            // Some(Ok(t)) if is_identifier(t)    => Ok(t),
-            Some(t) => Err(ErrorKind::UnexpectedToken(t).into()),
-            _ => Err(ErrorKind::NotImplemented.into())
-            // Some(Err(e)) => Err(e),
-            // None => Err(ErrorKind::UnexpectedEof.into()),
-        };
-
-        identifier.and_then(|id| {
-            let assignment = match self.lexer.next() {
-                Some(Token { column: _, line: _, typ: TokenType::Equals }) => self.assignment_expression(),
-                _ => Err(ErrorKind::UnexpectedEof.into())
-            };
-
-            assignment.map(|assignment| {
-                Node::Binding(
-                    Binding {
-                        identifier: id,
-                        assignment: Some(assignment),
-                    }
-                )
-            })
-        })
-    }
-
-    fn assignment_expression(&mut self) -> Result<Assignment> {
-        Err(ErrorKind::NotImplemented.into())
-    }
-
-    fn function_declaration(&mut self) -> Result<Node> {
-        Err(ErrorKind::NotImplemented.into())
-    }
-
-    fn class_declaration(&mut self) -> Result<Node> {
-        Err(ErrorKind::NotImplemented.into())
-    }
-
-    fn empty_statement(&mut self) -> Result<Node> {
-        Ok(Node::Statement)
-    }
-
-    fn block(&mut self) -> Result<Node> {
-        Err(ErrorKind::NotImplemented.into())
-    }
-
-    fn if_statement(&mut self) -> Result<Node> {
-        Err(ErrorKind::NotImplemented.into())
-    }
-
-    fn continue_statement(&mut self) -> Result<Node> {
-        Err(ErrorKind::NotImplemented.into())
-    }
-
-    fn break_statement(&mut self) -> Result<Node> {
-        Err(ErrorKind::NotImplemented.into())
-    }
-
-    fn return_statement(&mut self) -> Result<Node> {
-        Err(ErrorKind::NotImplemented.into())
-    }
-
-    fn with_statement(&mut self) -> Result<Node> {
-        Err(ErrorKind::NotImplemented.into())
-    }
-
-    fn throw_statement(&mut self) -> Result<Node> {
-        Err(ErrorKind::NotImplemented.into())
-    }
-
-    fn try_statement(&mut self) -> Result<Node> {
-        Err(ErrorKind::NotImplemented.into())
-    }
-
-    fn debugger_statement(&mut self) -> Result<Node> {
-        Err(ErrorKind::NotImplemented.into())
-    }
-
-    fn switch_statement(&mut self) -> Result<Node> {
-        Err(ErrorKind::NotImplemented.into())
-    }
-
-    fn do_statement(&mut self) -> Result<Node> {
-        Err(ErrorKind::NotImplemented.into())
-    }
-
-    fn while_statement(&mut self) -> Result<Node> {
-        Err(ErrorKind::NotImplemented.into())
-    }
-
-    fn for_statement(&mut self) -> Result<Node> {
-        Err(ErrorKind::NotImplemented.into())
+        Script { body: statements }
     }
 }
 
-fn is_variable_declaration(token: &Token) -> bool {
-    match token.typ {
-        TokenType::Identifier(_, Some(ReservedWord::Let))
-        | TokenType::Identifier(_, Some(ReservedWord::Const)) => true,
-        _ => false,
-    }
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-fn is_function_declaration(token: &Token) -> bool {
-    match token.typ {
-        TokenType::Identifier(_, Some(ReservedWord::Async))
-        | TokenType::Identifier(_, Some(ReservedWord::Function)) => true,
-        _ => false,
+    #[test]
+    fn parses_empty_script() {
+        let res = parse("");
+        assert_eq!(res.body.len(), 0);
     }
-}
 
-fn is_class_declaration(token: &Token) -> bool {
-    match token.typ {
-        TokenType::Identifier(_, Some(ReservedWord::Class)) => true,
-        _ => false,
+    #[test]
+    fn parses_empty_statement() {
+        let res = parse(";");
+        assert_eq!(res.body.len(), 1);
     }
-}
 
-fn is_variable_statement(token: &Token) -> bool {
-    match token.typ {
-        TokenType::Identifier(_, Some(ReservedWord::Var)) => true,
-        _ => false,
-    }
-}
-
-fn is_reserved_word(token: &Token, expected: &ReservedWord) -> bool {
-    match &token.typ {
-        TokenType::Identifier(_, Some(actual)) if actual == expected => true,
-        _ => false,
+    fn parse(input: &str) -> Script {
+        let lexer = Lexer::new(input.chars());
+        let mut parser = Parser::new(lexer);
+        parser.script()
     }
 }
